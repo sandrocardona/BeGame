251efc3a787b3cced9b46ea9c898356f601ab785
sandrocardona <sandrocardona41@gmail.com>
2024-02-29 17:43:18 +0100

    29 febrero

---
+++ jq/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    /* hacer click en div para redirigir a la ruta especificada */
+    $(".tarjeta-partidos-annadir").click(function(){
+        window.location.href = "../vistas/annadirpartido.html";
+    })
+    /* flecha hacia atrás */
+    $("#arrow-back").click(function(){
+        window.history.back();
+    })
+
+    /* logo header */
+    $("#logo").click(function(){
+        window.location.href = "../index.html";
+    })
+
+    /* Mostrar mensaje al clickar boton Enviar en footer */
+         // Recuperar el mensaje almacenado en localStorage al cargar la página
+        var mensajeGuardado = localStorage.getItem('mensaje');
+        if (mensajeGuardado) {
+            $("#mensaje").text(mensajeGuardado).show();
+            setTimeout(function(){
+                $("#mensaje").hide();
+                localStorage.removeItem('mensaje');
+            }, 3000);
+        }
+
+        $("#btnEnviar").click(function(){
+            // Guardar el mensaje en localStorage
+            var mensaje = "¡Formulario enviado con éxito!";
+            localStorage.setItem('mensaje', mensaje);
+        })
+    })
